217a8752d4b8f14d00f721815fd45c1763693f7c
龙鸣 <ming.long@hand-china.com>
2018-08-16 20:51:18 +0800

    项目提交

--- src/main/java/com/yibai/web/security/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/com/yibai/web/security/AuthenticationSuccessHandlerImpl.java
@@ -1,6 +1,10 @@
 package com.yibai.web.security;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
